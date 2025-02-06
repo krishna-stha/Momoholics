a7a3275cba5d2d392029c7676fbec21af9cbd237
Krishna Sharan Shrestha <89288382+krishna-stha@users.noreply.github.com>
2025-02-06 22:55:54 +0545

    Add files via upload
    Added the main HTML and CSS with just the navigation bar and the home section.

---
+++ Momoholics/script.js
@@ -0,0 +1,12 @@
+// function triggerEffect() {
+//     const box = document.getElementById('box');
+//     box.classList.toggle('active');
+//   }
+
+//   function trig(){
+//     const box = document.getElementById('box');
+//     box.classList.toggle('active');
+//   }
+  
+
+
